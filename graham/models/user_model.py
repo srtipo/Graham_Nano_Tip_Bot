9e3f9f3e5b32c435d8568f31e9f3b8c40045f0f1
srtipo <42049455+srtipo@users.noreply.github.com>
2018-08-07 20:36:26 +0200

    add news classmethods

--- graham/models/user_model.py
+++ graham/models/user_model.py
@@ -25,6 +25,41 @@ class User(BaseModel):
             return user
         return None
 
+    @classmethod
+    # To find user by id
+    def get_user_by_id(id):
+	try:
+               user = User.get(id==id)
+                return user
+        except User.DoesNotExist:
+                return None
+	
+    @classmethod
+    # To find user by name
+    def get_user_by_name(user_name):
+        try:
+            user = User.get(user_name=user_name)
+            return user
+        except User.DoesNotExist:
+            return None
+
+    @classmethod
+     # To find user by name wallet address
+    def get_user_by_wallet_address(address):
+        try:
+            user = User.select().join(Account).where(Account.wallet_address == address)
+            return user
+    except User.DoesNotExist:
+        return None
+
+    @classmethod
+    # To find wallet address by user
+    def get_address(user_id):
+        query=Account.select().join(User).where(User.id == user_id)
+        for account in query:
+            address=account.wallet_address
+            return address
+
 class UserRelationshipType(IntEnum):
     Favorites = 1
     Muted = 2
